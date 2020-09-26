dfb5b1b6e34cb695377af7e8e243b3b086a0fe8a
patrick-doumont <patrick.doumont@outlook.com>
2020-09-26 14:44:36 -0400

    completed through exercise 2.4(d)

--- exercise_files/ch2_exercises.R
+++ exercise_files/ch2_exercises.R
@@ -34,3 +34,32 @@ a * b
 
 ##Exercise 2.3
 #a
+(my_vec1 <- seq(from=5, to=-11, by=-0.3))
+
+#b
+(my_vec1 <- sort(my_vec1, decreasing = FALSE))
+
+#c
+(my_vec2 <- rep(c(-1, 3, -5, 7, -9), times=2, each=10))
+(my_vec2 <- sort(x=my_vec2, decreasing = TRUE))
+
+#d
+i_vec <- seq(from=6, to=12)
+ii_vec <- rep(5.3, times=3)
+iii_vec <- c(-3)
+iv_vec <- seq(from=102, to=length(my_vec2), length.out = 9)
+answer_vec <- c(i_vec, ii_vec, iii_vec, iv_vec)
+answer_vec
+
+##Exercise 2.4
+#a
+(a_vec <- c(seq(from=3, to=6, length.out = 5), rep(c(2,-5.1,-33),times=2), (7/42+2)))
+
+#b
+b_obj <- a_vec[c(1,length(a_vec))]
+b_obj
+#c
+c_obj <- a_vec[-c(1, length(a_vec))]
+c_obj
+#d
+(d_obj <- c(b_obj[1], c_obj, b_obj[2]))
